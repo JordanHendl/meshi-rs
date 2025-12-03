fd4cec6c447e03c5c7eb8f090b22f7e348d4ce59
Jordan Hendl <jordiehendl@gmail.com>
2025-12-02 21:11:30 -0600

    add more detail to scene module

--- graphics/src/render/scene.rs
+++ graphics/src/render/scene.rs
@@ -1,87 +1,260 @@
 use std::ptr::NonNull;
 
 use arrayvec::ArrayVec;
-use dashi::{Buffer, CommandStream, Context, Handle, cmd::Recording};
-use furikake::{types::{Camera, Material}, GPUState};
+use dashi::{
+    BindGroup, BindTable, Buffer, BufferInfo, BufferUsage, CommandStream, ComputePipeline, Context,
+    Handle, MemoryVisibility,
+    cmd::Recording,
+    driver::command::Dispatch,
+    utils::gpupool::{DynamicGPUPool, GPUPool},
+};
+use furikake::{
+    GPUState,
+    types::{Camera, Material},
+};
 use glam::Mat4;
 use noren::meta::DeviceModel;
 use resource_pool::resource_list::ResourceList;
-
-use crate::{RenderObject, RenderObjectInfo};
-
-const MAX_CHILDREN: usize = 18;
-pub struct SceneObject {
-    model: DeviceModel,
-    parent: Option<Handle<SceneObject>>,
-    children: ArrayVec<Handle<SceneObject>, MAX_CHILDREN>,
-    local_transform: Mat4,
-    world_transform: Mat4,
+#[repr(C)]
+pub struct SceneObjectInfo {
+    pub local: Mat4,
+    pub global: Mat4,
+    pub scene_mask: u32,
 }
 
-const MAX_NUM_MESHES: usize = 32;
 #[repr(C)]
-pub struct CulledMesh {
-    vertices: Handle<Buffer>,
-    indices: Handle<Buffer>,
-    material: Handle<Material>,
+pub struct SceneObject {
+    scene_mask: u32,
+    parent: Handle<SceneObject>,
+    local_transform: Mat4,
+    world_transform: Mat4,
+    children: ArrayVec<Handle<SceneObject>, 16>,
 }
 
 #[repr(C)]
 pub struct CulledObject {
-    transform: Mat4,
-    meshes: ArrayVec<CulledMesh, MAX_NUM_MESHES>,
+    total_transform: Mat4,
 }
+
+#[repr(C)]
+pub struct SceneBin {
+    pub id: u32,
+    pub mask: u32,
+}
+
+pub struct GPUSceneLimits {
+    pub max_num_scene_objects: u32,
+}
+
+pub struct GPUSceneInfo<'a> {
+    pub name: &'a str,
+    pub ctx: *mut Context,
+    pub draw_bins: &'a [SceneBin],
+}
+
+impl<'a> Default for GPUSceneInfo<'a> {
+    fn default() -> Self {
+        Self {
+            name: Default::default(),
+            ctx: Default::default(),
+            draw_bins: Default::default(),
+        }
+    }
+}
+
+struct GPUScenePipelineData {
+    pipeline: Handle<ComputePipeline>,
+    bind_groups: ArrayVec<Option<Handle<BindGroup>>, 4>,
+    bind_tables: ArrayVec<Option<Handle<BindTable>>, 4>,
+    curr_camera: Handle<Buffer>,
+}
+
 pub struct GPUScene<State: GPUState> {
     state: NonNull<State>,
     ctx: NonNull<Context>,
-    resources: ResourceList<RenderObject>,
+    scene_bins: Handle<Buffer>, // A buffer of scene bin descriptions... this is used to know which
+    // bins to put each scene object into when it passes the cull test.
+    objects_to_process: GPUPool<SceneObject>, // Scene objects to be culled.
+    draw_bins: DynamicGPUPool,                // In format [0..num_bins][0..max_bin_size] but flat.
+    pipeline_data: GPUScenePipelineData,
+    camera: Handle<Camera>,
 }
 
 impl<State: GPUState> GPUScene<State> {
-    pub fn new(ctx: &mut Context, state: &mut State) -> Self {
-        if State::reserved_names().iter().find(|name| **name == "meshi_bindless_materials") == None {
-            // Throw error result here.... we NEED meshi_bindless_materials for material listings.
-            panic!()
-        }
+    fn make_bento_shader() -> bento::CompilationResult {
+        let compiler = bento::Compiler::new().unwrap();
 
-        if State::reserved_names().iter().find(|name| **name == "meshi_bindless_camera") == None {
-            // Throw error result here.... we NEED meshi_bindless_materials for material listings.
-            panic!()
-        }
-
-        todo!("Initialize compute pipelines for GPU based culling. allocate data for outputs/intermediary steps")
+        return compiler
+            .compile(
+                &[],
+                &bento::Request {
+                    name: todo!(),
+                    lang: todo!(),
+                    stage: todo!(),
+                    optimization: todo!(),
+                    debug_symbols: todo!(),
+                },
+            )
+            .unwrap();
     }
-    
+
+    pub fn new(info: &GPUSceneInfo, state: &mut State) -> Self {
+        let ctx: &mut Context = unsafe { &mut (*info.ctx) };
+        if State::reserved_names()
+            .iter()
+            .find(|name| **name == "meshi_bindless_materials")
+            == None
+        {
+            // Throw error result here.... we NEED meshi_bindless_materials for material listings.
+            panic!()
+        }
+
+        if State::reserved_names()
+            .iter()
+            .find(|name| **name == "meshi_bindless_camera")
+            == None
+        {
+            // Throw error result here.... we NEED meshi_bindless_materials for material listings.
+            panic!()
+        }
+
+        let scene_bin_size = std::mem::size_of::<SceneBin>() * info.draw_bins.len();
+
+        Self {
+            state: NonNull::new(state).unwrap(),
+            ctx: NonNull::new(info.ctx).unwrap(),
+            objects_to_process: GPUPool::new(
+                ctx,
+                &BufferInfo {
+                    debug_name: todo!(),
+                    byte_size: todo!(),
+                    visibility: todo!(),
+                    usage: todo!(),
+                    initial_data: todo!(),
+                },
+            )
+            .unwrap(),
+            draw_bins: DynamicGPUPool::new(
+                ctx,
+                &BufferInfo {
+                    debug_name: todo!(),
+                    byte_size: todo!(),
+                    visibility: todo!(),
+                    usage: todo!(),
+                    initial_data: todo!(),
+                },
+                0,
+                0,
+            )
+            .unwrap(),
+            camera: Default::default(),
+            scene_bins: ctx
+                .make_buffer(&BufferInfo {
+                    debug_name: &format!("{} Draw Bin Descriptions", info.name),
+                    byte_size: scene_bin_size as u32,
+                    visibility: MemoryVisibility::Gpu,
+                    usage: BufferUsage::STORAGE,
+                    initial_data: unsafe { Some(info.draw_bins.align_to::<u8>().1) },
+                })
+                .expect(""),
+            pipeline_data: todo!(),
+        }
+    }
+
     pub fn set_active_camera(&mut self, camera: Handle<Camera>) {
         todo!("Set active camera in furikake state's active camera resources to use.")
     }
 
-    pub fn register_object(&mut self, info: &RenderObjectInfo) -> Handle<RenderObject> {
-        todo!("Add object to scene objects (to be used for frustum/LOD culling)")
+    pub fn register_object(&mut self, info: &SceneObjectInfo) -> Handle<SceneObject> {
+        self.objects_to_process
+            .insert(SceneObject {
+                local_transform: info.local,
+                world_transform: info.global,
+                scene_mask: info.scene_mask,
+                parent: Default::default(),
+                children: Default::default(),
+            })
+            .unwrap()
     }
 
-    pub fn release_object(&mut self, handle: Handle<RenderObject>) {
-        todo!("Release object from scene objects")
+    pub fn release_object(&mut self, handle: Handle<SceneObject>) {
+        self.objects_to_process.release(handle);
     }
 
-    pub fn transform_object(&mut self, handle: Handle<RenderObject>, transform: &Mat4) {
+    pub fn transform_object(&mut self, handle: Handle<SceneObject>, transform: &Mat4) {
+        self.objects_to_process
+            .get_mut_ref(handle)
+            .expect("")
+            .local_transform *= *transform;
         todo!("Modify scene object transform")
     }
 
-    pub fn set_object_transform(&mut self, handle: Handle<RenderObject>, transform: &Mat4) {
+    pub fn set_object_transform(&mut self, handle: Handle<SceneObject>, transform: &Mat4) {
+        self.objects_to_process
+            .get_mut_ref(handle)
+            .expect("")
+            .local_transform = *transform;
         todo!("Set scene object transform")
     }
 
-    pub fn add_child(&mut self, parent: Handle<RenderObject>, child: Handle<RenderObject>) {
-        todo!("Add child/parent relationship")
+    pub fn add_child(&mut self, parent: Handle<SceneObject>, child: Handle<SceneObject>) {
+        self.objects_to_process
+            .get_mut_ref(parent)
+            .expect("")
+            .children
+            .push(child);
+        self.objects_to_process.get_mut_ref(child).expect("").parent = parent;
     }
 
-    pub fn draw_list(&mut self) -> (CommandStream<Recording>, Handle<Buffer>) {
+    pub fn remove_child(&mut self, parent: Handle<SceneObject>, child: Handle<SceneObject>) {
+        let mut child_idx: i32 = -1;
+        for (i, ch) in self
+            .objects_to_process
+            .get_mut_ref(parent)
+            .expect("")
+            .children
+            .iter()
+            .enumerate()
+        {
+            if child == *ch {
+                child_idx = i as i32;
+                break;
+            }
+        }
+
+        if child_idx != -1 {
+            self.objects_to_process
+                .get_mut_ref(parent)
+                .unwrap()
+                .children
+                .remove(child_idx as usize);
+        }
+    }
+
+    pub fn cull(&mut self) -> (CommandStream<Recording>, &DynamicGPUPool) {
+        let mut stream = CommandStream::new().begin();
+        self.objects_to_process.sync_up(&mut stream);
+
+        stream = stream
+            .dispatch(&Dispatch {
+                x: todo!(),
+                y: todo!(),
+                z: todo!(),
+                pipeline: todo!(),
+                bind_groups: todo!(),
+                bind_tables: todo!(),
+                dynamic_buffers: todo!(),
+            })
+            .unbind_pipeline();
+
+        self.draw_bins.sync_down(&mut stream);
+
+        return (stream, &self.draw_bins);
         todo!(
-            "Solves parent/children via compute shader, record operations. Return a buffer containing the list of meshes to draw (with data)"
+            "Records compute shader dispatches to the gpu, returns ref to dynamic gpu pool. Pool is formatted like [0..num_bins][0..max_bin_meshes] of types CullResult"
         )
 
-            // Idea is: Either user can pull this in a full GPU driven renderer with inderect
-            // drawing.... or they can just pull to CPU and then iterate through draw list.
+        // Idea is: Either user can pull this in a full GPU driven renderer with inderect
+        // drawing.... or they can just pull to CPU and then iterate through draw list.
     }
 }
